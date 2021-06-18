c893f86c9e305914a69292d8154efe80f3d4f1ff
KyungHyun Jovinus Lee <lkh256@gmail.com>
2021-06-18 17:16:43 +0900

    add pairwise test

--- Code/survival_kaplan_meier.py
+++ Code/survival_kaplan_meier.py
@@ -21,11 +21,34 @@ kmf.fit(durations=df_select.loc[df_select['ABRP_CRF_tertile'] == 'T3', 'delta_ti
 kmf.plot_survival_function(ax=ax)
 
 plt.title('Kaplan Meier Curve for estimated CRF Tertile')
+
+# %%
+kmf = KaplanMeierFitter()
+
+fig, ax = plt.subplots(1,1, figsize=(8, 8))
+
+kmf.fit(durations=df_select.loc[df_select['ABRP_CRF_tertile'] == 'T1', 'delta_time'], event_observed=df_select.loc[df_select['ABRP_CRF_tertile'] == 'T1', 'death'], label='Q1')
+kmf.survival_function_.plot(ax=ax)
+
+kmf.fit(durations=df_select.loc[df_select['ABRP_CRF_tertile'] == 'T2', 'delta_time'], event_observed=df_select.loc[df_select['ABRP_CRF_tertile'] == 'T2', 'death'], label='Q2')
+kmf.survival_function_.plot(ax=ax)
+
+kmf.fit(durations=df_select.loc[df_select['ABRP_CRF_tertile'] == 'T3', 'delta_time'], event_observed=df_select.loc[df_select['ABRP_CRF_tertile'] == 'T3', 'death'], label='Q3')
+kmf.survival_function_.plot(ax=ax)
+
+plt.title('Kaplan Meier Curve for estimated CRF Tertile')
 # %%
 from lifelines.statistics import multivariate_logrank_test
 
 results = multivariate_logrank_test(event_durations=df_select['delta_time'],
                                     groups=df_select['ABRP_CRF_tertile'],
                                     event_observed=df_select['death'])
+print(results.print_summary)
+
+# %%
+from lifelines.statistics import pairwise_logrank_test
+results = pairwise_logrank_test(event_durations=df_select['delta_time'],
+                                groups=df_select['ABRP_CRF_tertile'],
+                                event_observed=df_select['death'])
 print(results.print_summary)
 # %%
